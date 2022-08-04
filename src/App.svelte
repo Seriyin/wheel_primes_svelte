55a39ef73dd9b9385b706adfb2507600de63ad04
André Diogo <redan.blue27@gmail.com>
2022-08-04 17:04:26 +0100

    Adds segmented sieve implementation and formats tables.

--- src/App.svelte
+++ src/App.svelte
@@ -1,46 +1,149 @@
 <script lang="ts">
-	import {Content} from 'carbon-components-svelte';
+	import {Content, Table, TableCell, TableContainer, TableRow} from 'carbon-components-svelte';
 	import {chunked} from './utils'
 	export let bindings;
 
 	let init_siever = 100
 	let init_primes = 7
 
-	$: p_sieved =  (bindings.sieve_pritchard(init_siever) as number[])
+	$: p_sieved_pritchard =  (bindings.sieve_pritchard_w(init_siever) as number[])
 
-	$: n_p_primes = (bindings.n_primes_pritchard(init_primes) as number[])
+	$: n_p_primes_pritchard = (bindings.n_primes_pritchard_w(init_primes) as number[])
+
+	$: p_sieved_segmented = (bindings.sieve_segmented_w(init_siever) as number[])
+
+	$: n_p_primes_segmented = (bindings.n_primes_segmented_w(init_primes) as number[])
+
 </script>
 
 <Content>
-	<h1>The Prime-Sieving App</h1>
-
-	<p>Sieving by limit</p>
-	<input type=number bind:value={init_siever}>
- 	<p>Sieve Result: </p>
-	<ol>
-		{#each chunked(p_sieved, 10) as sieved}
-			<li> {sieved} </li>
-		{/each}	
-	</ol>
+	<div>
+		<h1>The Prime-Sieving App</h1>
+	</div>
 
 
-	<p>Sieving N primes</p>
-	<input type=number bind:value={init_primes}>
-	<p>Sieve Result: </p>
-	<ol>
-		{#each chunked(n_p_primes, 10) as sieved}
-			<li> {sieved} </li>
-		{/each}	
-	</ol>
+	<div>
+		<h3>Sieving by limit</h3>
+		<input type=number bind:value={init_siever}>	
+	</div>
+
+	<div>
+		<h3>Sieving N primes</h3>
+		<input type=number bind:value={init_primes}>	
+	</div>
+
+	<div>
+		<h2>Pritchard Wheel Sieve</h2>
+	</div>
+	
+	<div>
+		<h3>Sieving by limit</h3>
+
+		<TableContainer>
+			<Table size="compact">
+				{#each chunked(p_sieved_pritchard, 10) as row}
+					<TableRow>
+						{#each row as cell}
+							<TableCell>{cell}</TableCell>
+						{/each}
+					</TableRow>
+				{/each}	
+				
+			</Table>
+		</TableContainer>
+	</div>
+	
+
+	<div>
+		<h3>Sieving N primes</h3>
+
+		<TableContainer>
+			<Table size="compact">
+				{#each chunked(n_p_primes_pritchard, 10) as row}
+					<TableRow>
+						{#each row as cell}
+							<TableCell>{cell}</TableCell>
+						{/each}
+					</TableRow>
+				{/each}	
+				
+			</Table>
+		</TableContainer>	
+	</div>
+
+	<div>
+		<h2>Segmented Erastothenes Sieve</h2>
+	</div>
+
+	<div>
+		<h3>Sieving by limit</h3>
+
+		<TableContainer>
+			<Table size="compact">
+				{#each chunked(p_sieved_segmented, 10) as row}
+					<TableRow>
+						{#each row as cell}
+							<TableCell>{cell}</TableCell>
+						{/each}
+					</TableRow>
+				{/each}	
+				
+			</Table>
+		</TableContainer>	
+	</div>
+
+
+	<div>
+		<h3>Sieving N primes</h3>
+
+
+		<TableContainer>
+			<Table size="compact">
+				{#each chunked(n_p_primes_segmented, 10) as row}
+					<TableRow>
+						{#each row as cell}
+							<TableCell>{cell}</TableCell>
+						{/each}
+					</TableRow>
+				{/each}	
+				
+			</Table>
+		</TableContainer>	
+	</div>
+
 
 </Content>
 
 <style>
+	:root {
+		height: auto;
+	}
+
 	h1 {
 		color: #ff3e00;
 		text-transform: uppercase;
-		font-size: 4em;
+		font-size: 5em;
 		font-weight: 100;
+		margin-top: 1%;
+		margin-bottom: 3%;
+	}
+
+	h2 {
+		color: coral;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 200;
+		margin-top: 2%;
+		margin-bottom: 2%;
+	}
+
+	h3 {
+		color: lightpink;
+		text-transform: capitalize;
+		font-size: xx-large;
+		font-weight: 100;
+		margin-top: 1%;
+		margin-bottom: 1%;
 	}
 
 	@media (min-width: 640px) {}
